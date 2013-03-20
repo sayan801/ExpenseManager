dcc618e2c5ce667b82161630e682c883332c385b
Joysankar Sengupta <joy.mcet@yahoo.com>
2013-03-20 13:04:48 +0530

    comment done fix the prob

--- code/desktop/ExpenseManagerData/ExpenseInfo.cs
+++ code/desktop/ExpenseManagerData/ExpenseInfo.cs
@@ -6,111 +6,112 @@ using System.Text;
 namespace ExpenseManagerData
 {
 
-    public enum ExpenseType
-    {
-        Fixed,
-        Variable,
-        OverDraft,
-        Adhoc
-    }
 
-    public enum EarningType
-    {
-        Fixed,
-        Variable,
-        Adhoc
-    }
+    //public enum ExpenseType
+    //{
+    //    Fixed,
+    //    Variable,
+    //    OverDraft,
+    //    Adhoc
+    //}
 
-    public enum SyncType
-    {
-        Web,
-        Mobile,
-        Desktop
-    }
+    //public enum EarningType
+    //{
+    //    Fixed,
+    //    Variable,
+    //    Adhoc
+    //}
 
-    public enum UserType
-    {
-        Admin,
-        Normal,
-        Guest
-    }
-    public enum SessionType
-    {
-        ValidUser,
-        InvalidUser,
-        TimeOut
-    }
+    //public enum SyncType
+    //{
+    //    Web,
+    //    Mobile,
+    //    Desktop
+    //}
 
-    public enum ReportType
-    {
-        Single,
-        Daily,
-        Weekly,
-        Monthly,
-        Quarterly,
-        Yearly
-    }
+    //public enum UserType
+    //{
+    //    Admin,
+    //    Normal,
+    //    Guest
+    //}
+    //public enum SessionType
+    //{
+    //    ValidUser,
+    //    InvalidUser,
+    //    TimeOut
+    //}
 
-    public class ExpenseInfo
-    {
-        public string id { get; set; }
-        public DateTime date { get; set; }
-        public ExpenseType type { get; set; }
-        public string description { get; set; }
-        public double amount { get; set; }
-        public bool IsWithInLimit;
-    }
+    //public enum ReportType
+    //{
+    //    Single,
+    //    Daily,
+    //    Weekly,
+    //    Monthly,
+    //    Quarterly,
+    //    Yearly
+    //}
 
-    public class EarningInfo
-    {
-        public string id { get; set; }
-        public DateTime date { get; set; }
-        public EarningType type { get; set; }
-        public string description { get; set; }
-        public double amount { get; set; }
-    }
+    //public class ExpenseInfo
+    //{
+    //    public string id { get; set; }
+    //    public DateTime date { get; set; }
+    //    public ExpenseType type { get; set; }
+    //    public string description { get; set; }
+    //    public double amount { get; set; }
+    //    public bool IsWithInLimit;
+    //}
 
-    public class SyncInfo
-    {
-        public string id { get; set; }
-        public DateTime date { get; set; }
-        public SyncType type { get; set; }
-        public string description { get; set; }
-        public float size { get; set; }
-    }
+    //public class EarningInfo
+    //{
+    //    public string id { get; set; }
+    //    public DateTime date { get; set; }
+    //    public EarningType type { get; set; }
+    //    public string description { get; set; }
+    //    public double amount { get; set; }
+    //}
 
-    public class UserInfo
-    {
-        public string id { get; set; }
-        public string encryptedPassword { get; set; }
-        public string name { get; set; }
-        public DateTime doj { get; set; }
-        public string address { get; set; }
-        public string phone { get; set; }
-        public List<ExpenseInfo> expenses { get; set; }
-        public double expenseLimit { get; set; }
-        public List<EarningInfo> earnings { get; set; }
-        public List<SyncInfo> syncDetails { get; set; }
-        public SessionInfo sessionDetails { get; set; }        
-    }
+    //public class SyncInfo
+    //{
+    //    public string id { get; set; }
+    //    public DateTime date { get; set; }
+    //    public SyncType type { get; set; }
+    //    public string description { get; set; }
+    //    public float size { get; set; }
+    //}
 
-    public class ReportInfo
-    {
-        public string id { get; set; }
-        public DateTime date { get; set; }
-        public ReportType type { get; set; }
-        public string description { get; set; }
-        public UserInfo user;
-    }
+    //public class UserInfo
+    //{
+    //    public string id { get; set; }
+    //    public string encryptedPassword { get; set; }
+    //    public string name { get; set; }
+    //    public DateTime doj { get; set; }
+    //    public string address { get; set; }
+    //    public string phone { get; set; }
+    //    public List<ExpenseInfo> expenses { get; set; }
+    //    public double expenseLimit { get; set; }
+    //    public List<EarningInfo> earnings { get; set; }
+    //    public List<SyncInfo> syncDetails { get; set; }
+    //    public SessionInfo sessionDetails { get; set; }        
+    //}
 
-    public class SessionInfo
-    {
-        public string id { get; set; }
-        public SessionType type { get; set; }
-        public int noOfloginAttempt { get; set; }
-        public int maxloginAttempt { get; set; }
-        public DateTime start { get; set; }
-        public DateTime end { get; set; }       
-    }
+    //public class ReportInfo
+    //{
+    //    public string id { get; set; }
+    //    public DateTime date { get; set; }
+    //    public ReportType type { get; set; }
+    //    public string description { get; set; }
+    //    public UserInfo user;
+    //}
+
+    //public class SessionInfo
+    //{
+    //    public string id { get; set; }
+    //    public SessionType type { get; set; }
+    //    public int noOfloginAttempt { get; set; }
+    //    public int maxloginAttempt { get; set; }
+    //    public DateTime start { get; set; }
+    //    public DateTime end { get; set; }       
+    //}
 
 }
